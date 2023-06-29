c18b7326508011e67a30a020f8237c3eb586b79c
Hamsa Jama <hamsa.jama@cazoo.co.uk>
2023-06-29 20:34:49 +0100

    Feature: Updated main.rs to support batch operation in the CLI

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use std::io;
 use std::env;
 use std::path::PathBuf;
 
+use data_ferret::db::Data;
 use data_ferret::db::Database;
+use data_ferret::db::OperationType;
 
 fn main() {
     // Use the first command-line argument for the database file
@@ -19,7 +21,8 @@ fn main() {
         println!("1. Insert data");
         println!("2. Retrieve data");
         println!("3. Delete data");
-        println!("4. Exit");
+        println!("4. Batch insert");
+        println!("5. Exit");
 
         let mut choice = String::new();
         io::stdin().read_line(&mut choice).expect("Failed to read line");
@@ -44,10 +47,40 @@ fn main() {
                     Err(e) => println!("Failed to delete data: {}", e),
                 }
             },
-            Ok(4) => break,
+            Ok(4) => {
+                let data = get_batch_data_from_user();
+                db.batch(data).expect("Failed to insert data in batch");
+            },
+            Ok(5) => break,
             _ => println!("Invalid choice"),
         }
     }
+}
+fn get_batch_data_from_user() -> Vec<Data> {
+    let mut data = Vec::new();
+    loop {
+        println!("Enter a partition key (or 'done' to finish):");
+        let mut partition_key = String::new();
+        io::stdin().read_line(&mut partition_key).expect("Failed to read line");
+        let partition_key = partition_key.trim().to_string();
+        if partition_key == "done" {
+            break;
+        }
+
+        println!("Enter a sort key:");
+        let mut sort_key = String::new();
+        io::stdin().read_line(&mut sort_key).expect("Failed to read line");
+        let sort_key = sort_key.trim().to_string();
+
+        println!("Enter a value:");
+        let mut value = String::new();
+        io::stdin().read_line(&mut value).expect("Failed to read line");
+        let value = value.trim().to_string();
+
+        data.push(Data { operation_type: OperationType::Insert, partition_key, sort_key, value });
+    }
+
+    data
 }
 
 fn get_data_from_user() -> (String, String, String) {
